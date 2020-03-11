3e6d2612aeba0cbeb13485518ad06bd3a7537158
Mathiot Daniel <daniel.mathiot@insa-cvl.fr>
2020-03-11 14:37:44 +0100

    Initial Commit to share the project

---
+++ stock/database/db.php
@@ -0,0 +1,19 @@
+<?php
+class DBConnect
+{
+    private static $instance=NULL;
+    public static function getInstance()
+    {
+        if (!isset(self::$instance)) {
+            self::$instance = new PDO("mysql:host=127.0.0.1;dbname=db1", "root", "azerty", NULL);
+        } //end if
+        return self::$instance;
+    } //end function
+
+    public static function closeInstance()
+    {
+        if (isset(self::$instance)) {
+            self::$instance=NULL;
+        }
+    }
+}// en class
